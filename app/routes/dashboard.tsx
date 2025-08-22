b81be9476367b0db92a6b90124fbafe59cfc0a6d
Agustin Benito <agusbenito2002@gmail.com>
2025-08-21 22:24:54 -0300

    varios comando de administracion

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -10,6 +10,28 @@ export default function Dashboard() {
 			</Menu>
 		);
 
+		const ventas = (
+			<Menu>
+				<Menu.Item key="add" onClick={() => alert("registrar comprovante de venta")}>Registrar comprovante de venta</Menu.Item>
+				<Menu.Item key="edit" onClick={() => alert("crear comprovante de venta")}>Crear comprovante de venta</Menu.Item>
+			</Menu>
+		);
+
+		const maquinaria = (
+			<Menu>
+				<Menu.Item key="add" onClick={() => alert("Administrar maquinaria")}>Administrar Maquinaria</Menu.Item>
+				<Menu.Item key="edit" onClick={() => alert("registrar mantenimiento")}>Registrar Mantenimiento</Menu.Item>
+			</Menu>
+		);
+
+		const proveedores = (
+			<Menu>
+				<Menu.Item key="add" onClick={() => alert("Agregar proveedor")}>Agregar comprovante de compra</Menu.Item>
+				<Menu.Item key="edit" onClick={() => alert("Modificar proveedor")}>Modificar Proveedor</Menu.Item>
+				<Menu.Item key="edit" onClick={() => alert("crear presupuesto")}>Crear Presupuesto</Menu.Item>
+			</Menu>
+		);
+
 		return (
 			<SidebarLayout>
 				<h1 style={{ fontSize: 32, fontWeight: "bold" }}>Dashboard</h1>
@@ -18,6 +40,20 @@ export default function Dashboard() {
 					<Dropdown overlay={menu} trigger={["click"]}>
 						<Button type="primary">Administración de Clientes</Button>
 					</Dropdown>
+					
+					<Dropdown overlay={ventas} trigger={["click"]}>
+						<Button type="primary">Administración de ventas</Button>
+					</Dropdown>
+
+					<Dropdown overlay={maquinaria} trigger={["click"]}>
+						<Button type="primary">Administración de maquinas</Button>
+					</Dropdown>
+
+					<Dropdown overlay={proveedores} trigger={["click"]}>
+						<Button type="primary">Administración de proveedores</Button>
+					</Dropdown>
+
+					
 				</div>
 				{/* Aquí puedes agregar widgets, estadísticas, gráficos, etc. */}
 			</SidebarLayout>
